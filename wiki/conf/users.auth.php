1162b5d1628988b1447b97107ba4dbcdbd2239ba
Cpalacio <capalacio68@misena.edu.co>
2016-05-10 07:13:36 -0500

    sistema informacion
    siap

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+mrosado:$1$59a4f4p0$zIWTV0jq6rD4TwT5zXBSc1:Melany Rosado:mrosado@empresastransportesjg.com:admin,user
